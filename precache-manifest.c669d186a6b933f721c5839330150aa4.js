65524ba9f4533047fe00458abfba1f4dc025bafc
wle8300 <email@wle8300.com>
2019-05-21 10:10:35 -0700

    Updates

---
+++ precache-manifest.c669d186a6b933f721c5839330150aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c51ff668766de375dac263c624f8e9",
+    "url": "/uifx-demo/index.html"
+  },
+  {
+    "revision": "a726527b1010c30826e1",
+    "url": "/uifx-demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "80b23c99c1ea4292715f",
+    "url": "/uifx-demo/static/js/2.682964ec.chunk.js"
+  },
+  {
+    "revision": "a726527b1010c30826e1",
+    "url": "/uifx-demo/static/js/main.3c295c27.chunk.js"
+  },
+  {
+    "revision": "d4c8d32674a92848aee0",
+    "url": "/uifx-demo/static/js/runtime~main.04759a03.js"
+  },
+  {
+    "revision": "6df49e4b4d380649bcf4f5fb313558bd",
+    "url": "/uifx-demo/static/media/beep.6df49e4b.mp3"
+  },
+  {
+    "revision": "eb69c905c327d591a01106e86e35cf79",
+    "url": "/uifx-demo/static/media/tick.eb69c905.mp3"
+  }
+]);
